3494e31f260bb7d17c4aacc1fb407c8d391a9709
Bryan Chan <bryan.chan@ualberta.ca>
2026-04-01 03:34:48 -0600

    Update scripts

---
+++ cc_scripts/slurm/run_hint_rl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --account=aip-schuurma
+#SBATCH --time=23:59:00
+#SBATCH --mem=100GB
+#SBATCH --cpus-per-task=2
+#SBATCH --gres=gpu:l40s:4
+#SBATCH --array=1-1
+#SBATCH --output=/home/chanb/scratch/logs/hint_rl/%j.out
+
+module load StdEnv/2023
+module load python/3.10.13
+module load cuda/12.9
+source /home/chanb/research/hint_rl/hint_rl/.venv/bin/activate
+
+python /home/chanb/research/hint_rl/hint_rl/cc_scripts/openmath_rl.py --config /home/chanb/research/hint_rl/hint_rl/cc_scripts/openmath_hint_rl_grpo.yaml
