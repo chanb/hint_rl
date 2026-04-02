2368d6c219bae2b1b97e563e99decfc37a3f33ed
Bryan Chan <bryanchan13@gmail.com>
2026-04-02 10:14:02 -0600

    Update

--- cc_scripts/slurm/run_hint_rl-continue_from_163.sh
+++ cc_scripts/slurm/run_hint_rl-continue_from_163.sh
@@ -12,4 +12,4 @@ module load python/3.10.13
 module load cuda/12.9
 source /home/chanb/research/hint_rl/hint_rl/.venv/bin/activate
 
-python /home/chanb/research/hint_rl/hint_rl/cc_scripts/openmath_rl.py --config /home/chanb/research/hint_rl/hint_rl/cc_scripts/openmath_hint_rl_grpo.yaml
+python /home/chanb/research/hint_rl/hint_rl/cc_scripts/openmath_rl.py --config /home/chanb/research/hint_rl/hint_rl/cc_scripts/openmath_hint_rl_grpo.yaml actor.path=/home/chanb/scratch/hint_rl_results/checkpoints/chanb/openmath-dapo/local_train-hint_rl/default/epoch11epochstep9globalstep163 trial_name=local_train-hint_rl-continue_from_163
