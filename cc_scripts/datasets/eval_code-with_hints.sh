c6d4c665edb120b0d8ddb942592e7e5585acc26d
Bryan Chan <bryan.chan@ualberta.ca>
2026-04-09 14:25:03 -0600

    update

--- cc_scripts/datasets/eval_code-with_hints.sh
+++ cc_scripts/datasets/eval_code-with_hints.sh
@@ -12,7 +12,7 @@ module load cuda/12.9
 source /home/chanb/research/hint_rl/hint_rl/.venv/bin/activate
 
 python /home/chanb/research/hint_rl/hint_rl/cc_scripts/eval_code-with_hints.py \
-    --config /home/chanb/research/hint_rl/hint_rl/cc_scripts/eval_code.yaml \
+    --config /home/chanb/research/hint_rl/hint_rl/cc_scripts/configs/eval/eval_code.yaml \
     trial_name=local_eval-evaluate_code-50_hints-openreasoning-nemotron \
     actor.path=nvidia/OpenReasoning-Nemotron-1.5B \
     valid_dataset.path=/home/chanb/scratch/datasets/opencode/data/opencode_hint_sep
