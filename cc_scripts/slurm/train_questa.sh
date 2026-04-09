ce972d859dcd907bf9eefae9762b150af175bf66
Bryan Chan <bryan.chan@ualberta.ca>
2026-04-09 14:09:29 -0600

    update scripts

--- cc_scripts/slurm/train_questa.sh
+++ cc_scripts/slurm/train_questa.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=aip-schuurma
-#SBATCH --time=01:00:00
-#SBATCH --mem=100GB
+#SBATCH --time=72:00:00
+#SBATCH --mem=400GB
 #SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:4
 #SBATCH --array=1-1
